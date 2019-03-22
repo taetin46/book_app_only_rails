facb2f604f8bca46f3b9d811946789ab2ebd84ae
taetin <taetin46@gmail.com>
2019-03-22 18:03:48 +0900

    CRUDの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,23 +8,38 @@ class BooksController < ApplicationController
   end
 
   def new
-  end
-
-  def create
+    @book = Book.new
   end
 
   def edit
+    @book = Book.find(book_show_params)
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def update
+    book = Book.find(book_show_params)
+    book.update(book_params)
+    redirect_to books_path
   end
 
   def destroy
+    book = Book.find(book_show_params)
+    book.destroy
+    redirect_to books_path
   end
 
   private
 
     def book_show_params
       params[:id]
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :author, :category)
     end
 end
